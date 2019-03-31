d0f53861a6fc80a9b21e4736b0247b83f54cff46
studentIvan <cryptox3@gmail.com>
2019-04-01 00:34:22 +0300

    Add brand header simulation and filtered simulation

--- src/app.js
+++ src/app.js
@@ -6,19 +6,72 @@ export default class App extends preact.Component {
   componentDidMount() {
     window.addEventListener('resize', () => this.forceUpdate());
   }
-  render({ products }) {
+  componentDidUpdate() {
+    console.log('rolll...');
+  }
+  render({ products, filtered }, { roll }) {
     return (
       <div id="app">
-        <header><h1>test header</h1></header>
+        <header>
+          <div class="dtbrandheader">
+            <div class="dtbrandheader__left">
+              <h1 class="dtbrandheader__header">Dope</h1>
+              <div class="dtbrandheader__description">A bad day riding is better than a good day at work!</div>
+            </div>
+            <div class="dtbrandheader__image" style="background-image: url(https://d10g92rh9h0kij.cloudfront.net/media/catalog/category/dopeheaderzermatt_1.jpg);" />
+          </div>
+          {/* <!-- simple click on the line below to change the data for the test --> */}
+          <div class="dtactions" onClick={ () => this.setState(prevState => ({ roll: !prevState.roll }))}>
+            <div class="dtactions__filterspanel">
+              <div class="dtselectbox">
+                <button aria-label="Department" class="filterblock">
+                  <div class="filterblock__label">Department</div>
+                  <div class="filterblock__plus">+</div>
+                  <div class="dtselectbox__picdn"></div>
+                </button>
+                <div class="dtselectbox__dropdown dtselectbox__dropdown--hidden">
+                  <div class="dtselectbox__positioner" style="">
+                    <div class="dtsortselector__dropdown">
+                      <button class="dtselectbox__selectbtn">Streetwear</button>
+                      <button class="dtselectbox__selectbtn">Snow</button>
+                      <button class="dtselectbox__selectbtn">Skate</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="dtactions__sortpanel">
+              <div class="dtselectbox">
+                <button class="dtsortselector" aria-label="Sort by">
+                  <span class="dtsortselector__sortbytext" data-e2e="sort">Sort by</span>
+                  <span class="dtsortselector__sortbylabel">Hot products</span>
+                  <div class="dtselectbox__picdn"></div>
+                </button>
+                <div class="dtselectbox__dropdown dtselectbox__dropdown--hidden">
+                  <div class="dtselectbox__positioner" style="">
+                    <div class="dtsortselector__dropdown">
+                      <button class="dtselectbox__selectbtn">Cheapest first</button>
+                      <button class="dtselectbox__selectbtn">Latest first</button>
+                      <button class="dtselectbox__selectbtn">Hot products</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+
         <RSVirtualListX
-          className="vlist"
-          innerWidth={ window.innerWidth }
-          itemsBufferLength={ 6 }
-          rowBuffer={ 2 }
-          items={ products }
+          className="vlist" // vlist classname
+          innerWidth={ window.innerWidth } // resize updates
+          itemsBufferLength={ 6 } // initial items count
+          rowBuffer={ 2 } // render visible rows + this to bottom and to top
+          items={ !roll ? products : filtered } // products
+          uniqueListKey={ !roll ? 1 : 2 } // force re-rendering on change
           itemRenderFn={ item => <Item key={ item.id } item={ item } /> }
           plugRenderFn={ item => <a key={ item.id } className="rs-products-list-item" /> }
         />
+
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './app';
 let link = null;
 (async () => {
   const { products } = await (await fetch('https://www.ridestore.com/rest-api/v2/categories/1057?lang=en')).json();
+  const filtered = await (await fetch('https://www.ridestore.com/rest-api/v2/categories/409?lang=en&filter[129]=18')).json();
   /** @jsx preact.h */
-  link = preact.render(<App products={ products } />, document.body);
+  link = preact.render(<App products={ products } filtered={ filtered.products } />, document.body);
 })();
