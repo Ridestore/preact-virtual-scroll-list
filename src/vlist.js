dc7e6e5c051986e852a0791f6322ece3f07ce0e5
Ivan Maslov <cryptox3@gmail.com>
2021-09-07 10:55:19 +0300

    Make the code of vlist actual

--- src/vlist.js
+++ src/vlist.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-plusplus */
-import preact from 'preact';
+import { Component } from 'preact';
 
 /** @type {Window} */
 const wnd = typeof window !== 'undefined' ? window : false;
@@ -8,9 +8,9 @@ const raf = wnd && (wnd.requestAnimationFrame ||
   wnd.webkitRequestAnimationFrame ||
   wnd.mozRequestAnimationFrame ||
   wnd.msRequestAnimationFrame ||
-  wnd.oRequestAnimationFrame);
+  wnd.oRequestAnimationFrame).bind(wnd);
 
-export default class RSVirtualScrollList extends preact.Component {
+export default class RSVirtualScrollList extends Component {
   constructor(props) {
     super(props);
     this.scrollHandlerFn = this.scrollHandler.bind(this);
@@ -22,10 +22,10 @@ export default class RSVirtualScrollList extends preact.Component {
   }
 
   loop() {
-    const { rowBuffer } = this.props;
+    const { rowBuffer, scrollArea } = this.props;
     const { scrollBoxTop, viewBoxHeight, flexRowHeight, visibleRows, flexItemsPerRow } = this;
     /** calculate window scroll Y */
-    const viewBoxScrollY = wnd.scrollY;
+    const viewBoxScrollY = scrollArea ? scrollArea.scrollTop : wnd.scrollY;
 
     /** visible area - user's visible area rectangle */
     const visibleAreaTop = viewBoxScrollY >= scrollBoxTop ? 0 : scrollBoxTop - viewBoxScrollY;
@@ -57,8 +57,9 @@ export default class RSVirtualScrollList extends preact.Component {
     if (this.state.endRow !== endRow) {
       this.setState((prevState) => {
         if (prevState.endRow !== endRow) {
+          const _items = this.getBufferElements(sliceStart, sliceEnd);
           return {
-            itemsBuffer: this.getBufferElements(sliceStart, sliceEnd),
+            itemsBuffer: _items,
             endRow,
           };
         }
@@ -74,6 +75,7 @@ export default class RSVirtualScrollList extends preact.Component {
   componentWillReceiveProps(nextProps) {
     if (this.props.uniqueListKey && nextProps.uniqueListKey !== this.props.uniqueListKey) {
       this.state.endRow = -1; // this action will force items re-rendering
+      this.props.items = nextProps.items;
     }
     this.recalculateData();
   }
@@ -82,33 +84,50 @@ export default class RSVirtualScrollList extends preact.Component {
     const { items } = this.props;
     /** calculate static values */
     this.viewBoxHeight = wnd.innerHeight;
+    if (!this.base) return;
     this.scrollBoxTop = this.base.offsetTop;
 
     /** calculate static flex row length */
-    const grid = Array.from(this.base.children);
-    const baseOffset = grid[0].offsetTop;
+    const grid = this.base && this.base.children ? Array.from(this.base.children) : [];
+    const baseOffset = grid[0] ? grid[0].offsetTop : 0;
     const breakIndex = grid.findIndex(item => item.offsetTop > baseOffset);
     this.flexItemsPerRow = (breakIndex === -1 ? grid.length : breakIndex);
     this.rowsCount = Math.ceil(items.length / this.flexItemsPerRow);
 
     /** calculate static values */
     const betweenColumnsGap = 5;
-    this.flexItemClientRect = grid[0].getBoundingClientRect();
-    this.flexRowHeight = this.flexItemClientRect.height + (betweenColumnsGap / 2);
+    this.flexItemClientRect = grid[0] ? grid[0].getBoundingClientRect() : null;
+    this.flexRowHeight = this.flexItemClientRect ? this.flexItemClientRect.height + (betweenColumnsGap / 2) : 0;
     this.scrollBoxHeight = this.flexRowHeight * this.rowsCount;
     this.visibleRows = Math.ceil(this.viewBoxHeight / this.flexRowHeight);
+
+    if (this.flexRowHeight > 1500 && !this.recalculatedTwice) {
+      this.recalculatedTwice = setTimeout(() => this.recalculateData(), 200);
+    }
 
     /** trigger first loop */
     this.scrollHandler();
   }
 
   getBufferElements(sliceStart, sliceEnd) {
-    const { itemRenderFn, items, plugRenderFn } = this.props;
+    const { itemRenderFn, items, plugRenderFn, onItemsReqCallback } = this.props;
     const bufferElements = [];
     /** for ... i++ is faster than map or forEach with slice */
+    const _sliceStart = sliceStart > ((items.length - 1) - this.flexItemsPerRow)
+      ? (items.length - 1) - this.flexItemsPerRow : sliceStart;
     for (let i = 0, x = items.length; i < x; i++) {
-      bufferElements.push(i < sliceStart || i > sliceEnd
-        ? plugRenderFn(items[i]) : itemRenderFn(items[i]));
+      bufferElements.push(i < _sliceStart || i > sliceEnd
+        ? plugRenderFn(items[i], i) : itemRenderFn(items[i], i));
+    }
+    if (onItemsReqCallback) {
+      const visibleItemsCount = this.visibleRows * this.flexItemsPerRow;
+      if (!Number.isNaN(visibleItemsCount)) {
+        onItemsReqCallback({
+          sliceStart: _sliceStart,
+          sliceEnd,
+          visibleItemsCount,
+        });
+      }
     }
     return bufferElements;
   }
@@ -117,25 +136,55 @@ export default class RSVirtualScrollList extends preact.Component {
     const sliceEnd = this.props.itemsBufferLength || 6;
     this.state.itemsBuffer = this.getBufferElements(0, sliceEnd);
     this.state.endRow = -1;
+
+    /** inner integrity tests */
+    const { items, keyFunction, itemRenderFn, plugRenderFn } = this.props;
+    switch (true) {
+      case this.props.scrollArea === wnd:
+        throw new Error('RSVirtualScrollList scrollArea shouldn\'t be a Window, use null instead');
+      case !this.props.uniqueListKey:
+        throw new Error('RSVirtualScrollList should have a uniqueListKey property');
+      case !itemRenderFn:
+        throw new Error('RSVirtualScrollList should have a itemRenderFn function property');
+      case !plugRenderFn:
+        throw new Error('RSVirtualScrollList should have a plugRenderFn function property');
+      case !keyFunction:
+        throw new Error('RSVirtualScrollList should have a keyFunction function property');
+      case items && items.length && keyFunction(items[0]) === keyFunction(items[0], true):
+        throw new Error('RSVirtualScrollList keyFunction should return the different values for the fake objects');
+      case items && items.length && items[0] && items[1] && keyFunction(items[0]) === keyFunction(items[1]):
+        throw new Error('RSVirtualScrollList keyFunction should return the different values for the objects');
+      default: break;
+    }
   }
 
   componentDidMount() {
+    const scrollArea = this.props.scrollArea || wnd;
     /** no need these calculations for ssr */
-    if (!wnd) return;
-    /** setup passive scroll listener */
-    wnd.addEventListener('scroll', this.scrollHandlerFn, { passive: true });
-    this.recalculateData();
+    if (!wnd && !scrollArea) return;
+    if (wnd && typeof wnd.document.createElement('div').style.grid === 'string') {
+      /** setup passive scroll listener */
+      scrollArea.addEventListener('scroll', this.scrollHandlerFn, { passive: true });
+      this.recalculateData();
+      if (this.scrollBoxTop > 1000) {
+        setTimeout(() => this.recalculateData(), 100);
+      }
+    }
+    else {
+      setTimeout(() => {
+        this.setState({
+          itemsBuffer: this.getBufferElements(0, this.props.items.length - 1),
+        });
+      });
+    }
   }
 
   componentWillUnmount() {
-    if (wnd) { wnd.removeEventListener('scroll', this.scrollHandlerFn); }
+    const scrollArea = this.props.scrollArea || wnd;
+    if (scrollArea) { scrollArea.removeEventListener('scroll', this.scrollHandlerFn); }
   }
 
-  render({ className }, { itemsBuffer }) {
-    return (
-      <section className={ className } id="vlist">
-        { itemsBuffer }
-      </section>
-    );
+  render({ children }, { itemsBuffer }) {
+    return { ...children[0], children: itemsBuffer };
   }
 }
